c61b5e12b932de7169293210e4ea7dc713a451b3
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-08-15 15:45:12 +0530

    pushing

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,13 @@ import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
-  const [todos , setTodos] = useState([{'todo' : 'Learn React', 'Completed' : false},
-{'todo' : 'Build a React app', 'Completed' : false},{'todo' : 'Deploy the React app', 'Completed' : false}])
+  const [todos , setTodos] = useState([{'todo' : 'Learn React', 'mark' : 'incomplete'},
+{'todo' : 'Build a React app', 'mark' : 'incomplete'},{'todo' : 'Deploy the React app', 'mark' : 'incomplete'}])
 
 function completedHandler(index){
     const updatedList = todos.map((item,idx) => {
       if(idx === index){
-        return {...item,Completed : true}
+        return {...item,mark : 'completed'}
     }
     else return item
   });
@@ -25,9 +25,10 @@ function completedHandler(index){
         <ul>
           {
           todos.map((item,idx) => {
+            // console.log(item);
             return(
               <li key={idx}>{item.todo }
-             { !item.Completed && <button onClick={()=>completedHandler(idx)}>Complete</button>}</li>
+             { item.mark !== 'completed' && <button onClick={()=>completedHandler(idx)}>Complete</button>}</li>
             )
           })
           }
